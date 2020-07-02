99a4e45b5750453be2a99bcd7bfed5b7c8092fb9
Quang Đặng <quangdangfit@gmail.com>
2020-07-02 10:11:16 +0700

    refactor interfaces

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -4,35 +4,8 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"go-banking/helpers"
+	"go-banking/interfaces"
 )
-
-type User struct {
-	gorm.Model
-	Username string
-	Email    string
-	Password string
-}
-
-type Account struct {
-	gorm.Model
-	Type    string
-	Name    string
-	Balance uint
-	UserID  uint
-}
-
-type ResponseAccount struct {
-	ID      uint
-	Name    string
-	Balance int
-}
-
-type ResponseUser struct {
-	ID       uint
-	Username string
-	Email    string
-	Accounts []ResponseAccount
-}
 
 func connectDB() *gorm.DB {
 	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=postgres password=1234 sslmode=disable")
@@ -43,25 +16,28 @@ func connectDB() *gorm.DB {
 func createAccounts() {
 	db := connectDB()
 
-	users := [2]User{
+	users := &[2]interfaces.User{
 		{Username: "Martin", Email: "martin@martin.com"},
 		{Username: "Michael", Email: "michael@michael.com"},
 	}
 
 	for i := 0; i < len(users); i++ {
+		// Correct one way
 		generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
-		user := User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
+		user := &interfaces.User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
 		db.Create(&user)
 
-		account := Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
+		account := &interfaces.Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
 		db.Create(&account)
 	}
 	defer db.Close()
 }
 
 func Migrate() {
+	User := &interfaces.User{}
+	Account := &interfaces.Account{}
 	db := connectDB()
-	db.AutoMigrate(&User{}, &Account{})
+	db.AutoMigrate(&User, &Account)
 	defer db.Close()
 
 	createAccounts()
