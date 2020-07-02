575ab43c30795d4f6816bfad3edf42f42fadb57f
Quang Đặng <quangdangfit@gmail.com>
2020-07-02 10:05:21 +0700

    start project

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"go-banking/migrations"
+)
+
+func main() {
+	migrations.Migrate()
+}
---
+++ migrations/migrations.go
@@ -0,0 +1,55 @@
+package migrations
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"go-banking/helpers"
+)
+
+type User struct {
+	gorm.Model
+	Username string
+	Email    string
+	Password string
+}
+
+type Account struct {
+	gorm.Model
+	Type    string
+	Name    string
+	Balance uint
+	UserID  uint
+}
+
+func connectDB() *gorm.DB {
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=postgres password=1234 sslmode=disable")
+	helpers.HandleErr(err)
+	return db
+}
+
+func createAccounts() {
+	db := connectDB()
+
+	users := [2]User{
+		{Username: "Martin", Email: "martin@martin.com"},
+		{Username: "Michael", Email: "michael@michael.com"},
+	}
+
+	for i := 0; i < len(users); i++ {
+		generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
+		user := User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
+		db.Create(&user)
+
+		account := Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
+		db.Create(&account)
+	}
+	defer db.Close()
+}
+
+func Migrate() {
+	db := connectDB()
+	db.AutoMigrate(&User{}, &Account{})
+	defer db.Close()
+
+	createAccounts()
+}
