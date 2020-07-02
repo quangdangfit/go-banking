da2e7cdd339d4f0bc3c75f738c971443e97ceadb
Quang Đặng <quangdangfit@gmail.com>
2020-07-02 15:10:52 +0700

    Create function updateAccount

---
+++ useraccounts/useraccounts.go
@@ -0,0 +1,12 @@
+package useraccounts
+
+import (
+	"go-banking/helpers"
+	"go-banking/interfaces"
+)
+
+func updateAccount(id uint, amount int) {
+	db := helpers.ConnectDB()
+	db.Model(&interfaces.Account{}).Where("id = ?", id).Update("balance", amount)
+	defer db.Close()
+}
