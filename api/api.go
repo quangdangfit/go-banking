e3292834e3e0988a31a5f6dd3eb93e8f0e70ace4
Quang Đặng <quangdangfit@gmail.com>
2020-07-02 11:58:42 +0700

    api register

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"go-banking/helpers"
+	"go-banking/interfaces"
 	"go-banking/users"
 	"io/ioutil"
 	"log"
@@ -43,9 +44,30 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func register(w http.ResponseWriter, r *http.Request) {
+	// Read body
+	body, err := ioutil.ReadAll(r.Body)
+	helpers.HandleErr(err)
+	// Handle registration
+	var formattedBody interfaces.Register
+	err = json.Unmarshal(body, &formattedBody)
+	helpers.HandleErr(err)
+	register := users.Register(formattedBody.Username, formattedBody.Email, formattedBody.Password)
+	// Prepare response
+	if register["message"] == "all is fine" {
+		resp := register
+		json.NewEncoder(w).Encode(resp)
+		// Handle error in else
+	} else {
+		resp := ErrResponse{Message: "Wrong username or password"}
+		json.NewEncoder(w).Encode(resp)
+	}
+}
+
 func StartApi() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/register", register).Methods("POST")
 	fmt.Println("App is working on port :8888")
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
