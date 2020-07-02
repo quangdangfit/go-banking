fa47970ad069e51b38e83f03cf53c71977a16266
Quang Đặng <quangdangfit@gmail.com>
2020-07-02 13:53:45 +0700

    refactor register api

--- api/api.go
+++ api/api.go
@@ -12,20 +12,11 @@ import (
 	"net/http"
 )
 
-type Login struct {
-	Username string
-	Password string
-}
-
-type ErrResponse struct {
-	Message string
-}
-
 func login(w http.ResponseWriter, r *http.Request) {
 	// Refactor login to use readBody
 	body := readBody(r)
 
-	var formattedBody Login
+	var formattedBody interfaces.Login
 	err := json.Unmarshal(body, &formattedBody)
 	helpers.HandleErr(err)
 
@@ -35,23 +26,15 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	// Read body
-	body, err := ioutil.ReadAll(r.Body)
-	helpers.HandleErr(err)
-	// Handle registration
+	body := readBody(r)
+
 	var formattedBody interfaces.Register
-	err = json.Unmarshal(body, &formattedBody)
+	err := json.Unmarshal(body, &formattedBody)
 	helpers.HandleErr(err)
+
 	register := users.Register(formattedBody.Username, formattedBody.Email, formattedBody.Password)
-	// Prepare response
-	if register["message"] == "all is fine" {
-		resp := register
-		json.NewEncoder(w).Encode(resp)
-		// Handle error in else
-	} else {
-		resp := ErrResponse{Message: "Wrong username or password"}
-		json.NewEncoder(w).Encode(resp)
-	}
+	// Refactor register to use apiResponse function
+	apiResponse(register, w)
 }
 
 func readBody(r *http.Request) []byte {
