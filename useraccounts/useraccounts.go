736a6b335d8d772dcf3da1a2c521fa2b7a18179e
Quang Đặng <quangdangfit@gmail.com>
2020-07-03 09:06:12 +0700

    function get account

--- useraccounts/useraccounts.go
+++ useraccounts/useraccounts.go
@@ -5,6 +5,16 @@ import (
 	"go-banking/interfaces"
 )
 
+func getAccount(id uint) *interfaces.Account {
+	db := helpers.ConnectDB()
+	account := &interfaces.Account{}
+	if db.Where("id = ? ", id).First(&account).RecordNotFound() {
+		return nil
+	}
+	defer db.Close()
+	return account
+}
+
 func updateAccount(id uint, amount int) {
 	db := helpers.ConnectDB()
 	db.Model(&interfaces.Account{}).Where("id = ?", id).Update("balance", amount)
